f32eda4fec32671fab41db6180a028f47786b41a
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-03-29 18:20:23 +0200

    Added category controller.

---
+++ src/test/java/ua/vadym/spring5webfluxrestapi/controllers/CategoryControllerTest.java
@@ -0,0 +1,53 @@
+package ua.vadym.spring5webfluxrestapi.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ua.vadym.spring5webfluxrestapi.domain.Category;
+import ua.vadym.spring5webfluxrestapi.repositories.CategoryRepository;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class CategoryControllerTest {
+
+    private static final String BASE_URL = "/api/v1/categories/";
+    private WebTestClient webTestClient;
+    private CategoryRepository repository;
+
+    @Before
+    public void setUp() {
+        repository = mock(CategoryRepository.class);
+        CategoryController controller = new CategoryController(repository);
+        webTestClient = WebTestClient.bindToController(controller).build();
+    }
+
+    @Test
+    public void getAllCategories() {
+        Category fruits = Category.builder().description("Fruits").build();
+        Category nuts = Category.builder().description("Nuts").build();
+
+        given(repository.findAll()).willReturn(Flux.just(fruits, nuts));
+
+        webTestClient.get()
+                .uri(BASE_URL)
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getCategoryById() {
+        String id = "category id";
+        Category nuts = Category.builder().description("Nuts").build();
+        given(repository.findById(id)).willReturn(Mono.just(nuts));
+
+        webTestClient.get()
+                .uri(BASE_URL + id)
+                .exchange()
+                .expectBody(Category.class)
+                .isEqualTo(nuts);
+    }
+}
